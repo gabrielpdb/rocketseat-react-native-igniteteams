2802d24452261adbe768c7bfa47efa43b9a402fc
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-01-07 20:24:36 -0300

    Fontes personalizadas

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,13 +1,12 @@
+import { DefaultTheme } from "styled-components"
 import styled from "styled-components/native"
+
+type ThemeProps = {
+  theme: DefaultTheme
+}
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
-`
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
+  background-color: ${({ theme }: ThemeProps) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `
