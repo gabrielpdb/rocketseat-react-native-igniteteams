4d2c0af5db301af4471d8d146ebba3ed5396b8e2
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-01-24 22:45:29 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from "@components/Highlight"
 import { GroupCard } from "@components/GroupCard"
 import { useState } from "react"
 import { FlatList } from "react-native"
+import { ListEmpty } from "@components/ListEmpty"
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
@@ -21,6 +22,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   )
