5d3c0e501c59cc6e7a5fe7ceefa365279067c300
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-01-30 21:17:10 -0300

    Criando a interface players

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import theme from "@theme/index"
 
 import { Groups } from "@screens/Groups"
 import { NewGroup } from "@screens/NewGroup"
+import { Players } from "@screens/Players"
 
 import { Loading } from "@components/Loading"
 
@@ -22,7 +23,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   )
 }
