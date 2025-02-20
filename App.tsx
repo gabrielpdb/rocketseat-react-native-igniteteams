8aec28e1a57052d074d74cea17756bc0433b3d8a
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-02-19 21:01:04 -0300

    Estrutra de navegação

--- App.tsx
+++ App.tsx
@@ -7,11 +7,8 @@ import {
 } from "@expo-google-fonts/roboto"
 import theme from "@theme/index"
 
-import { Groups } from "@screens/Groups"
-import { NewGroup } from "@screens/NewGroup"
-import { Players } from "@screens/Players"
-
 import { Loading } from "@components/Loading"
+import { Routes } from "@routes/index"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -23,7 +20,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   )
 }
