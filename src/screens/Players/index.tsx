0b0bbbb081b4ea17a16054a717ded126728b8a41
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-03-05 06:22:31 -0300

    Carregando players pelo grupo e time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -13,6 +13,8 @@ import { useRoute } from "@react-navigation/native"
 import { AppError } from "@utils/AppError"
 import { playerAddByGroup } from "@storage/player/playerAddByGroup"
 import { playersGetByGroup } from "@storage/player/playersGetByGroup"
+import { playerGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam"
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO"
 
 type RouteParams = {
   group: string
@@ -21,7 +23,7 @@ type RouteParams = {
 export function Players() {
   const [newPlayerName, setNewPlayerName] = useState("")
   const [team, setTeam] = useState("Time A")
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
   const route = useRoute()
   const { group } = route.params as RouteParams
@@ -38,8 +40,6 @@ export function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group)
-      const players = await playersGetByGroup(group)
-      console.log(players)
     } catch (error) {
       if (error instanceof AppError) {
         Alert.alert("Nova pessoa", error.message)
@@ -48,6 +48,19 @@ export function Players() {
 
         Alert.alert("Nova pessoa", "Não foi possível adicionar")
       }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playerGetByGroupAndTeam(group, team)
+      setPlayers(playersByTeam)
+    } catch (error) {
+      console.log(error)
+      Alert.alert(
+        "Pessoas",
+        "Não foi possível carregar as pessoas filtradas do time selecionado"
+      )
     }
   }
 
